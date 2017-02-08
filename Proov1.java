949879edac8d0bb3fa1d2db9bef4bdcbca39e6d1
Mattias Blehner <matttias@hithub.com>
2017-02-08 13:20:13 +0200

    Tegin arvutuse ühe nurga leidmiseks

--- Proov1.java
+++ Proov1.java
@@ -1,8 +1,10 @@
 public class Proov1{
 	public static void main(String[] args){
-		TaisTrapets laud1=new TaisTrapets(2,1,3,8);
-		TaisTrapets laud2=new TaisTrapets(1.5,1,5.5,7.8);
-		TaisTrapets laud3=new TaisTrapets(2, 1.3, 5, 6);
+		TaisTrapets laud1=new TaisTrapets(2,1,3,8,6);
+		TaisTrapets laud2=new TaisTrapets(1.5,1,5.5,7.8,8.6);
+		TaisTrapets laud3=new TaisTrapets(2, 1.3, 5, 6, 9.8);
+		TaisTrapets laud4=new TaisTrapets(5, 6, 8, 8, 4);
+		
 		System.out.println(laud1.pindala());
 		System.out.println(laud2.pindala());
 		System.out.println(laud3.pindala());
@@ -18,6 +20,13 @@ public class Proov1{
 		System.out.println(laud1.pyt());
 		System.out.println(laud2.pyt());
 		System.out.println(laud3.pyt());
+		
+			System.out.println("---------");
+		
+		System.out.println(laud1.angle());
+		System.out.println(laud2.angle());
+		System.out.println(laud3.angle());
+		System.out.println(laud4.angle());
 		
 	}
 }
--- TaisTrapets.java
+++ TaisTrapets.java
@@ -1,16 +1,22 @@
 public class TaisTrapets{
-	double a, x, b, h;
-	public TaisTrapets(double kylg, double alus, double hypotenuus, double korgus){
+	double a, x, b, h, c;
+	public TaisTrapets(double kylg, double alus, double hypotenuus, double korgus, double teinealus){
 		if(kylg<=0){
 			throw new RuntimeException("Sobimatu");}
 		if(alus<=0){
 			throw new RuntimeException("Sobimatu");}
+		if(hypotenuus<=0){
+			throw new RuntimeException("Sobimatu");}
 		if(korgus<=0){
 			throw new RuntimeException("Sobimatu");}
+		if(teinealus<=0){
+			throw new RuntimeException("Sobimatu");}	
+			
 		a=kylg;
 		x=alus;
-		b=hypotenuus;
-		h=korgus;
+		b=hypotenuus;//ei ole vaja
+		h=korgus;//ei ole vaja
+		c=teinealus;
 	}
 	public double pindala(){
 		return (a*a)+(x*h/2);
@@ -23,4 +29,9 @@ public class TaisTrapets{
 	public double pyt(){
 		return (Math.sqrt(Math.pow(x+a, 2)+Math.pow(a, 2)));
 	}
+	//https://i.stack.imgur.com/X6SG6.jpg
+	public double angle(){
+		return (Math.toDegrees(Math.PI/2+Math.atan(c-x/a)));
+	}
+	
 }
